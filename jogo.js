0ceaa8e6c2ef22990800dc3cd111fe5a93660dd7
Karine Riquena Limp <karine_limp@hotmail.com>
2020-04-20 16:37:25 -0300

    Soco do Ryu e Colisão

--- jogo.js
+++ jogo.js
@@ -1,4 +1,6 @@
 console.log('[DevSoutinho] Flappy Bird');
+const som_HIT = new Audio();
+som_HIT.src = './efeitos/hit.wav';
 
 const sprites = new Image();
 sprites.src = './sprites.png';
@@ -63,6 +65,54 @@ const chao = {
   },
 };
 
+function fazColisao(flappyBird, chao) {
+  const flappyBirdY =  flappyBird.y + flappyBird.altura;
+  const chaoY = chao.y;
+  if (flappyBirdY >= chaoY) {
+    return true;
+  }
+
+  return false;
+}
+
+function criaFlappyBird() {
+  const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    pulo: 4.6,
+    pula() {
+      flappyBird.velocidade = -flappyBird.pulo;
+    },
+    atualiza() {
+      if (fazColisao(flappyBird, chao)) {
+        som_HIT.play();
+        setTimeout(() => {
+          mudaParaTela(Telas.INICIO);
+        }, 500);
+        return;
+      }
+      flappyBird.velocidade += flappyBird.gravidade;
+      flappyBird.y += flappyBird.velocidade;
+    },
+    desenha() {
+        // Desenhar vários quadros da tela a cada segundo = FPS
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite  
+            flappyBird.x, flappyBird.y, // Dentro do canvas, onde vai desenhar a imagem
+            flappyBird.largura, flappyBird.altura, // Dentro do canvas, qual vai ser o tamanho do desenho
+        );
+    }
+  }
+  return flappyBird;
+}
 // ["passarinho"]
 const flappyBird = {
     spriteX: 0,
@@ -73,7 +123,15 @@ const flappyBird = {
     y: 50,
     gravidade: 0.25,
     velocidade: 0,
+    pulo: 4.6,
+    pula() {
+      flappyBird.velocidade = -flappyBird.pulo;
+    },
     atualiza() {
+      if (fazColisao(flappyBird, chao)) {
+        mudaParaTela(Telas.INICIO);
+        return;
+      }
       flappyBird.velocidade += flappyBird.gravidade;
       flappyBird.y += flappyBird.velocidade;
     },
@@ -114,17 +172,24 @@ const mensagemGetReady = {
 //
 // Telas
 //
+const globais = {};
 let telaAtiva = {};
 function mudaParaTela(novaTela) {
   telaAtiva = novaTela;
+  if (telaAtiva.inicializa) {
+    telaAtiva.inicializa();
+  }
 }
 
 const Telas = {
   INICIO: {
+    inicializa() {
+      globais.flappyBird = criaFlappyBird();
+    },
     desenha() {
       planoDeFundo.desenha();
       chao.desenha();
-      flappyBird.desenha();
+      globais.flappyBird.desenha();
       mensagemGetReady.desenha();
     },
     click() {
@@ -140,10 +205,13 @@ Telas.JOGO = {
   desenha() { 
     planoDeFundo.desenha();
     chao.desenha();
-    flappyBird.desenha();
+    globais.flappyBird.desenha();
+  },
+  click() {
+    globais.flappyBird.pula();
   },
   atualiza() {
-    flappyBird.atualiza();
+    globais.flappyBird.atualiza();
   }
 }
 
